f7f7685c89196922dc1e02ad6a906e3330571277
beenbeanz <haileylan404@gmail.com>
2025-02-05 09:26:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,13 +24,33 @@ animal = input("input an animal\n")
 Madlib = f"One day, {celebrity} was {verb1} their {animal}. The {animal} had {number} friends of the same species, and they suddenly {verb2} at {celebrity}. However, {celebrity} had a {noun} in their bag, and attacked the animals. The {animal}s ran away from fright."
 print(Madlib)"""
 
-def oddOrEven():
-    number = input("Enter a number\n")
-    int(number)
-    number = number % 2
+"""def oddOrEven():
+    number = int(input("Enter a number\n")) % 2
     if (number == 0):
         print("That is an even number")
     elif (number == 1):
         print("That is an odd number")
-oddOrEven()
+oddOrEven()"""
+
+"""def tip(bill):
+    if bill == "bad":
+        print("You get a 0% tip")
+    elif bill == "okay":
+        print("You get a 15% tip")
+    elif bill == "good":
+        print("You get a 20% tip")
+    elif bill == "great":
+        print("You get a 25% tip")
+tip("bad")"""
+
+
+def factors(num):
+    factorsList = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            factorsList.append(i)
+print(factors(12))
+
+
+
 
