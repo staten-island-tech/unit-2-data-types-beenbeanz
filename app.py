b1d3f9912bb41a5648764fa0d7cf86426797939e
beenbeanz <haileylan404@gmail.com>
2025-02-05 08:45:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,34 @@ print(values[7])
 for i in values:
     print(i)"""
 
-x = "this is a thing"
+"""x = "this is a thing"
 y= x.split( )
 z = y[0]
 print(y)
-print(z)
+print(z)"""
+
+"""def getLength():   define function                          
+    msg = input("input a sentence \n")      get user input
+    print(len(msg))   print length of input
+getLength()"""
+
+"""verb1 = input("input a verb in present tense\n")
+verb2 = input("input a second verb in past tense\n")
+noun = input("input a noun\n")
+number = input("input a number\n")
+celebrity = input("input a celebrity's name\n")
+animal = input("input an animal\n")
+
+Madlib = f"One day, {celebrity} was {verb1} their {animal}. The {animal} had {number} friends of the same species, and they suddenly {verb2} at {celebrity}. However, {celebrity} had a {noun} in their bag, and attacked the animals. The {animal}s ran away from fright."
+print(Madlib)"""
+
+def oddOrEven():
+    number = input("Enter a number\n")
+    int(number)
+    number = number % 2
+    if (number == 0):
+        print("That is an even number")
+    elif (number == 1):
+        print("That is an odd number")
+oddOrEven()
+
