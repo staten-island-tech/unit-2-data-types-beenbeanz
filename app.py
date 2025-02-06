67cd4549ed13a61731eee581df143593988ce75a
beenbeanz <haileylan404@gmail.com>
2025-02-06 09:22:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import random
+
 """values = [1,2.23,5,7,2,30,15]
 print(values[7])
 for i in values:
@@ -44,13 +46,46 @@ oddOrEven()"""
 tip("bad")"""
 
 
-def factors(num):
+"""def factors(num):
     factorsList = []
     for i in range(1, num + 1):
         if num % i == 0:
             factorsList.append(i)
-print(factors(12))
+print(factors(12))"""
 
+"""def GCF(num1, num2):
+    factors1 = []
+    factors2 = []
+    commonFactors = []
+    for i in range(1, num1 + 1):
+        if num1 % i == 0:
+            factors1.append(i)
+    for j in range(1, num2 + 1):
+        if num2 % j == 0:
+            factors2.append(j)
+    for k in range(len(factors1)):
+        for l in range(len(factors2)):
+            if factors1[k] == factors2[l]:
+                commonFactors.append(factors2[l])
+    gcf = max(commonFactors)
+    return gcf
+print(GCF(2000, 1000))"""
 
-
-
+def numberGuessing():
+    guess_history = []
+    guess = 0
+    randomNum = random.randint(1, 10)
+    guess = int(input("Guess a number\n"))
+    while(guess != randomNum):
+        if guess < randomNum:
+            print("The random number is greater than your guess")
+        elif guess > randomNum:
+            print("The random number is less than your guess")
+        guess_history.append(guess)
+        print(f"Used numbers: {guess_history}")
+        guess = int(input("Guess a number\n"))
+    print(f"Correct! You guessed the number")
+    print("Guess history:")
+    for i in range(len(guess_history)):
+        print(guess_history[i])
+numberGuessing()
